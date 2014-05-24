927df694888c9485a4ef592e91300525dc0e0bb8
Michal Mikolas <nanuqcz@gmail.com>
2014-05-25 00:43:26 +0200

    Initial commit.

---
+++ Scaffolding/PresenterFactory.php
@@ -0,0 +1,29 @@
+<?php
+namespace Shake\Scaffolding;
+
+use \Shake\Application\UI\Presenter;
+use \Nette;
+
+
+/**
+ * Scaffolding\PresenterFactory
+ *
+ * @author  Michal Mikoláš <nanuqcz@gmail.com>
+ * @package Shake
+ */
+class PresenterFactory extends Nette\Application\PresenterFactory
+{
+
+	public function getPresenterClass(& $name)
+	{
+		// Default Nette presenter loading
+		try {
+			return parent::getPresenterClass($name);
+		
+		// Create virtual presenter
+		} catch (Nette\Application\InvalidPresenterException $e) {
+			return 'Shake\Scaffolding\Presenter';
+		}
+	}
+
+}
