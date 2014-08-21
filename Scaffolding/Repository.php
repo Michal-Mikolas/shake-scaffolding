8a397c5585e59e0f119c7b5860db9daf115b8f57
Michal Mikolas <nanuqcz@gmail.com>
2014-08-21 22:22:16 +0200

    Repository: Fixed automatic column prefix if using SQL function.

--- Scaffolding/Repository.php
+++ Scaffolding/Repository.php
@@ -416,7 +416,7 @@ class Repository extends Object
 	 */
 	private function prefix($columnName)
 	{
-		if (strpos($columnName, '.') || strpos($columnName, ':'))
+		if (strpos($columnName, '.') || strpos($columnName, ':') || ctype_upper($columnName{0}))
 			return $columnName;
 
 		return $this->getTableName() . ".$columnName";
